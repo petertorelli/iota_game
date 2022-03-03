eefca4070ece5129a1d04717d75839f24d84df5d
petertorelli <peter.j.torelli@gmail.com>
2022-03-02 16:40:34 -0800

    Cards are now bytes; improved search

--- components/CardImage.vue
+++ components/CardImage.vue
@@ -1,53 +1,41 @@
 <template lang='pug'>
-.card(
-    :style='{ color: mapColor(card.color) }'
-    v-html='mapShape(card.shape) + " " + card.score')
+.card(:style='{ color }' v-html='shape + " " + score')
 </template>
+
 <script lang='ts'>
-
 import Vue from 'vue';
-
-interface Dictionary {
-    [index: string]: string;
-}
-
-const colors: Dictionary = {
-    'blue': 'dodgerblue',
-    'red': 'red',
-    'yellow': 'gold',
-    'green': 'seagreen',
-    'white': 'white'
-};
-
-const shapes: Dictionary = {
-    'cross': '&#x271a;',
-    'circle': '&#x25cf;',
-    'triangle': '&#x25b2',
-    'square': '&#x25a0;',
-    '': '',
-}
+import * as card from './CardObject';
 
 export default Vue.component( "CardImage", {
-    props: {
-        card: {
-            type: Object,
-            default() {
-                return {
-                    color: 'white',
-                    shape: 'square',
-                    score: 0,
-                }
-            }
-        }
-    },
-    methods: {
-        mapShape(name: string) {
-            return shapes[name];
-        },
-        mapColor(name: string) {
-            return colors[name];
-        }
+  props: {
+    card: {
+      type: Number,
+      default: 0xff,
     }
+  },
+  computed: {
+    shape() {
+      return card.htmlShape(this.card);
+    },
+    color() {
+      return card.htmlColor(this.card);
+    },
+    score() {
+      return card.score(this.card);
+    }
+  }
 });
-
 </script>
+
+<style lang='scss' scoped>
+.card {
+  display: flex; 
+  align-items: center;
+  justify-content: space-around;
+  font-size: 1rem;
+  width: 40px;
+  height: 40px;
+  border: 1px solid #ccc;
+  margin: 3px;
+}
+</style>
